743824c6f351c72de2e1a7c98a37c67f244149e2
Anton Aŭtuška <antoninus.liberalis@gmail.com>
2026-05-21 23:27:42 +0300

    pass cached --prompt to run-shell (ctrl-b-f)

--- zsh.zsh
+++ zsh.zsh
@@ -4,7 +4,7 @@
 _harvest_pick() {
   setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2>/dev/null
   local selected
-  selected=$(tmux capture-pane -p -t "$TMUX_PANE" -S -10000 | /Users/anton/proj/harvest/target/release/harvest --cwd "$PWD" --prompt "$PROMPT" | fzf --reverse --height 40%)
+  selected=$(tmux capture-pane -p -t "$TMUX_PANE" -S -10000 | /Users/anton/proj/scrollback/target/release/harvest --cwd "$PWD" --prompt "$PROMPT" | fzf --reverse --height 40%)
   [[ -n "$selected" ]] && LBUFFER="${1}${selected}"
   zle reset-prompt
 }
@@ -12,3 +12,6 @@ _harvest_pick() {
 _harvest_widget() { _harvest_pick "$LBUFFER"; }
 zle -N _harvest_widget
 bindkey '^F' _harvest_widget
+
+mkdir -p "${XDG_CACHE_HOME:-$HOME/.cache}/harvest"
+print -r -- "$PROMPT" > "${XDG_CACHE_HOME:-$HOME/.cache}/harvest/prompt"
